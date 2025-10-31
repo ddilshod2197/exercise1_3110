11e86fa564a5bac440ad8b23e5a5697f97b7a2f7
ddilshod2197 <ddilshod2197@gmail.com>
2025-10-31 15:06:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+class Kompyuter:
+    def __init__(self, model, ram, disk_hajmi):
+        self.model = model
+        self.ram = ram
+        self.disk_hajmi = disk_hajmi
+
+    def malumot(self):
+        """Kompyuter ma'lumotlarini qaytaradi"""
+        return f"Model: {self.model}, RAM: {self.ram} GB, Disk hajmi: {self.disk_hajmi} GB"
+
+    def ram_yangila(self, yangi_ram):
+        """RAMni yangilaydi"""
+        self.ram = yangi_ram
+        print(f"RAM yangilandi: {self.ram} GB")
+
+komp1 = Kompyuter("Dell Inspiron", 8, 512)
+print(komp1.malumot())
+
+komp1.ram_yangila(16)
+print(komp1.malumot())
